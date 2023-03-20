c40739d15c668962c612c692f772ceb6d50f8e50
Vasko Nikolov <hugovasko1@gmail.com>
2023-03-20 23:34:58 +0200

    Melo-9: Add IMusicItem interface

--- Album.cs
+++ Album.cs
@@ -6,29 +6,35 @@ using System.Threading.Tasks;
 
 namespace Melodify
 {
-    public class Album : MusicItem
+    public class Album : IMusicItem
     {
+        public string Title { get; set; }
+        public int Duration { get; set; }
         public Artist Artist { get; set; }
         public Genre Genre { get; set; }
         public int Year { get; set; }
         public List<Song> Songs { get; set; }
 
-        public Album(string title, Artist artist, Genre genre, int year, int duration) : base(title, duration)
+        public Album(string title, Artist artist, Genre genre, int year, int duration)
         {
+            Title = title;
+            Duration = duration;
+            Songs = new List<Song>();
             Artist = artist;
             Genre = genre;
             Year = year;
-            Songs = new List<Song>();
         }
 
         public void AddSong(Song song)
         {
             Songs.Add(song);
+            Duration += song.Duration;
         }
 
         public void RemoveSong(Song song)
         {
             Songs.Remove(song);
+            Duration -= song.Duration;
         }
     }
 }
